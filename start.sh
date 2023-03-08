b3ad6046966b18b4d7829a8aa81310941e03234d
Noah Stolmaker @ Pi <nstolmaker@gmail.com>
2023-03-08 10:42:44 -0800

    i think this iss right, because thiss process is a subscriber and jusst runs all the time. it shouldnt need to be restarted unless it crasshess, in which case pm2 sshould be auto restarting it. also adding the timeoutworker here because its part of this package.

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-pm2 start "node -- main.js" --cron-restart="0/10 * * * *"
+pm2 start "node -- main.js" --name main 
+pm2 start "node -- timeoutWorker.js" --name timeoutWorker
+#pm2 start "node -- main.js" --cron-restart="0/10 * * * *"
